65346f12f1bce23a69d4d8b1d4843cf584a17361
anisamsrh <nuranisahidayatulmasruroh@gmail.com>
2023-08-01 22:25:50 +0700

    menerapkan web-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const config = require('./webpack.config');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(config, {
   mode: 'development', 
@@ -9,7 +10,14 @@ module.exports = merge(config, {
     filename: 'bundle.js',
     clean: true,
   },
-  watch: true,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    compress: true,
+    port: 9000,
+    liveReload: true,
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'main.css',
